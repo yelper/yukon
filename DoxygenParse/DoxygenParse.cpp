c442a3d67db20588a7dbe602a5ecf011813ba336
Alper Sarikaya <yelper@gmail.com>
2012-11-14 12:30:37 -0600

    Finishing the parsing of dot files.  Outputs a 'call graph' in vector form, with method names.  Still need to find a way to get file -> method name mappings.

--- DoxygenParse/DoxygenParse.cpp
+++ DoxygenParse/DoxygenParse.cpp
@@ -15,21 +15,21 @@ using std::make_shared;
 using std::enable_shared_from_this;
 
 #include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
 
 void printUsageAndExit(int exitCondition)
 {
     exit(exitCondition);
 }
 
-vector<vector<string> > parseCallGraph(string filename)
+vector<vector<int> > parseCallGraph(string filename, map<string, string> &names)
 {
     ifstream f(filename.c_str());
     if (!f.is_open())
         cerr << "Unable to open specified file: " << filename << endl;
     
-    vector<vector<string> > graph;
-    map<string, string> names;
     vector<vector<string> > lines;
+    names.clear();
     string line;
     while (f.good())
     {
@@ -56,12 +56,15 @@ vector<vector<string> > parseCallGraph(string filename)
     }
 
     // now that we have all the labels, we can fill them in.
+    vector<vector<int> > graph(names.size(), vector<int>(names.size(), 0));
     for (int i = 0; i < (int)lines.size(); i++)
     {
         vector<string> line = lines[i];
         if (line[1] == "->")
         {
-
+            int from = boost::lexical_cast<int>(line[0].substr(4, 1)) - 1;
+            int to   = boost::lexical_cast<int>(line[2].substr(4, 1)) - 1;
+            graph[from][to] = 1;
         }
     }
 
@@ -75,7 +78,18 @@ int main(int argc, char** argv)
         printUsageAndExit(-1);
     
     string fname = argv[1];
-	parseCallGraph(fname);
+    map<string, string> names;
+	vector<vector<int> > graph = parseCallGraph(fname, names);
+
+    for (int i = 0; i < graph.size(); i++)
+    {
+        for (int j = 0; j < graph.size(); j++)
+        {
+            cout << graph[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
 
---
+++ TestProg/TestProg.cpp
@@ -0,0 +1,32 @@
+// TestProg.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int overloaded(int i)
+{
+    return i + 1;
+}
+
+int overloaded(int i, int j)
+{
+    return i + j;
+}
+
+int main(int argc, char** argv)
+{
+    int first = 1;
+    int second = 2;
+
+    int third = overloaded(first);
+    int fourth = overloaded(third, second);
+
+    cout << "first: " << first << endl;
+    cout << "second: " << second << endl;
+    cout << "third: " << third << endl;
+    cout << "fourth: " << fourth << endl;
+}
+
+
