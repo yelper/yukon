ee5b0222d82c6741eb3862bea9b9f3a2f7263995
Alper Sarikaya <yelper@gmail.com>
2012-11-19 18:30:17 -0600

    Adding DoxygenParse.h file (missed in last commit)

---
+++ DoxygenParse/DoxygenParse.h
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <string>
+#include <map>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class DoxygenParse {
+public:
+    vector<vector<int> > parseCallGraph(string filename, map<string, string> &names);
+private:
+
+}
